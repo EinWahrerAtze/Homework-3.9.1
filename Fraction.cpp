25d2d1692ff9b12e91c2745ea3b44903890880d2
Denis Fedorov <adeptusmar_62@mail.ru>
2022-07-14 21:49:57 +0300

    Переработы операции == и <

--- Fraction.cpp
+++ Fraction.cpp
@@ -8,7 +8,18 @@ Fraction::Fraction(int numenator, int denominator)
 
 bool Fraction::operator==(const Fraction & f) const
 {
-	return (numenator_ / denominator_ == f.numenator_ / f.denominator_);
+	if (numenator_ == f.numenator_)
+	{
+		return (denominator_ == f.denominator_ ? true : false);
+	}
+	else if (denominator_ == f.denominator_)
+	{
+		return (numenator_ == f.numenator_ ? true : false);
+	}
+	else
+	{
+		return (numenator_ == denominator_ && f.numenator_ == f.denominator_ ? true : false);
+	}
 }
 
 bool Fraction::operator!=(const Fraction & f) const
@@ -18,7 +29,25 @@ bool Fraction::operator!=(const Fraction & f) const
 
 bool Fraction::operator<(const Fraction & f) const
 {
-	return (numenator_ / denominator_ < f.numenator_ / f.denominator_);
+	if (numenator_ == f.numenator_)
+	{
+		return (denominator_ < f.denominator_ ? true : false);
+	}
+	else if (denominator_ == f.denominator_)
+	{
+		return (numenator_ < f.numenator_ ? false : true);
+	}
+	else if (*this == f)
+	{
+		return false;
+	}
+	else
+	{
+		numenator_ * f.denominator_;
+		f.numenator_ * denominator_;
+
+		return (numenator_ < f.numenator_ ? false : true);
+	}
 }
 
 bool Fraction::operator>(const Fraction & f) const
